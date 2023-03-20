05c073b528fd0354232e81be8a75dbc9370a2057
JuanPabloDS <63618603+JuanPabloDS@users.noreply.github.com>
2023-03-20 13:50:57 -0300

    Editando SVG

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\drive\portifolio\portifolio\env
+set VIRTUAL_ENV=E:\suporte\drive\portifolio\env
 
 if not defined PROMPT set PROMPT=$P$G
 

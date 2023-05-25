c9dc94d791d797def31d709737fa970c0cadaebc
JuanPabloDS <63618603+JuanPabloDS@users.noreply.github.com>
2023-05-25 16:27:49 -0300

    Cadastrar os dados do projetos dinamicamente

---
+++ .history/core/admin_20230525141416.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Tecnologia, Projeto
+
+
+@admin.register(Tecnologia)
+class TecnologiaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'imagem','criado', 'modificado', 'ativo',)
